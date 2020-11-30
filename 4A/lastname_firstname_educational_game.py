fbb9cfd3e42b25b87d36057fa7ca2844cae2e295
Ryan Kelley <kelleyr@duvalschools.org>
2020-11-30 14:00:24 -0500

    4A v0.1
    Added supply variables.

--- 4A/lastname_firstname_educational_game.py
+++ 4A/lastname_firstname_educational_game.py
@@ -1,4 +1,4 @@
-# <Darwin's Journey to the Galapgos Islands>, <Ryan Kelley>, <1:51PM> <11/30/20>, <Version 0.05>
+# <Darwin's Journey to the Galapgos Islands>, <Ryan Kelley>, <2:00PM> <11/30/20>, <Version 0.1>
 
 # Global Variables
 
@@ -11,3 +11,18 @@ party_member0 = ""
 party_member1 = ""
 party_member2 = ""
 party_member3 = ""
+
+# Supply Variables
+amount_food = 0
+amount_water = 0
+num_ships = 0
+num_guns = 0
+num_bullets = 0
+num_swords = 0
+num_axes = 0
+num_horses = 0
+num_wagons = 0
+num_clothing = 0
+num_cows = 0
+num_chickens = 0
+num_pigs = 0 
