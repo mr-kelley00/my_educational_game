387c5737ffba54df01376c23817811d1f9877c6f
Ryan Kelley <kelleyr@duvalschools.org>
2020-11-30 13:17:42 -0500

    [3A] v0.1
    Added global variables needed by the game.

--- 3A/kelley_ryan_educational_game.py
+++ 3A/kelley_ryan_educational_game.py
@@ -1,2 +1,20 @@
-# <Lewis and Clark Expedition Extravaganza>, <Ryan Kelley>, <11/30/20> <1:03PM>, <Version 0.0>
+# <Lewis and Clark Expedition Extravaganza>, <Ryan Kelley>, <11/30/20> <1:17PM>, <Version 0.1>
 
+# Global Variables
+player_score = 0
+player_name = ""
+party_member0 = ""
+party_member1 = ""
+party_member2 = ""
+party_member3 = ""
+player_job = ""
+start_money = 0
+num_boats = 0
+num_guns = 0
+num_bullets = 0
+amount_food = 0
+amount_water = 0
+game_time = 0
+game_date = 0
+landmark_distance = 0
+total_distance = 0 
