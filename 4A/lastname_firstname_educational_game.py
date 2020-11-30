4180d17c935f2bca6945302014d33cb2573d4389
Ryan Kelley <kelleyr@duvalschools.org>
2020-11-30 13:52:41 -0500

    4A v0.05
    Added player variables.

--- 4A/lastname_firstname_educational_game.py
+++ 4A/lastname_firstname_educational_game.py
@@ -1,3 +1,13 @@
-# <Darwin's Journey to the Galapgos Islands>, <Ryan Kelley>, <1:42PM> <11/30/20>, <Version 0.0>
+# <Darwin's Journey to the Galapgos Islands>, <Ryan Kelley>, <1:51PM> <11/30/20>, <Version 0.05>
 
+# Global Variables
 
+# Player Variables
+occupation = ""
+start_money = 0
+score_bonus = 0
+player_name = ""
+party_member0 = ""
+party_member1 = ""
+party_member2 = ""
+party_member3 = ""
