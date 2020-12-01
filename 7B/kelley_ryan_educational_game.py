2242ff7df91bb450bdb7483233a9a1c9915c5c34
Ryan Kelley <kelleyr@duvalschools.org>
2020-12-01 12:53:38 -0500

    [7B] v0.1
    Added player variables.

--- 7B/kelley_ryan_educational_game.py
+++ 7B/kelley_ryan_educational_game.py
@@ -1,1 +1,12 @@
-# <Educational Game>, <Ryan Kelley>, <12:39PM> <12/01/20>, <Version 0.0>
+# <Educational Game>, <Ryan Kelley>, <12:52PM> <12/01/20>, <Version 0.1>
+
+# Player Variables
+player_role = ""
+starting_resources = 0
+score_bonus = 0
+player_name = ""
+party_member0 = ""
+party_member1 = ""
+party_member2 = ""
+party_member3 = ""
+party_member4 = ""
