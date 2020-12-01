95403e36b1545ed50fb172eb1d2276d20c46d7c7
Ryan Kelley <kelleyr@duvalschools.org>
2020-12-01 08:54:53 -0500

    [5B] v0.2
    Added inventory variables.

--- 5B/kelley_ryan_educational_game.py
+++ 5B/kelley_ryan_educational_game.py
@@ -1,4 +1,4 @@
-# <The Lewis and Clark Expedition Simulator>, <Ryan Kelley>, <8:38AM> <12/01/20>, <Version 0.1>
+# <The Lewis and Clark Expedition Simulator>, <Ryan Kelley>, <8:54AM> <12/01/20>, <Version 0.2>
 
 # Player Variables
 starting_role = ""
@@ -9,3 +9,16 @@ party_member0 = ""
 party_member1 = ""
 party_member2 = ""
 party_member3 = ""
+
+# Inventory Variables
+amt_water = 0
+amt_food = 0
+num_clothing = 0
+num_guns = 0
+num_bullets = 0
+amt_rope = 0
+num_horses = 0
+num_boats = 0
+num_wagons = 0
+num_wagon_wheel = 0
+num_wagon_axle = 0
