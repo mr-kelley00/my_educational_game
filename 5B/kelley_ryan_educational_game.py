ea871070dbb37b3673a70949d14cb6ad4f78f6f2
Ryan Kelley <kelleyr@duvalschools.org>
2020-12-01 09:04:37 -0500

    [5B] v0.3
    Added disaster variables.

--- 5B/kelley_ryan_educational_game.py
+++ 5B/kelley_ryan_educational_game.py
@@ -1,4 +1,4 @@
-# <The Lewis and Clark Expedition Simulator>, <Ryan Kelley>, <8:54AM> <12/01/20>, <Version 0.2>
+# <The Lewis and Clark Expedition Simulator>, <Ryan Kelley>, <9:04AM> <12/01/20>, <Version 0.3>
 
 # Player Variables
 starting_role = ""
@@ -22,3 +22,12 @@ num_boats = 0
 num_wagons = 0
 num_wagon_wheel = 0
 num_wagon_axle = 0
+
+# Disaster Variables, % chance it occurs.  
+chc_sick = 0
+chc_break_wheel = 0
+chc_break_axle = 0
+chc_hostile_native = 0
+chc_bad_weather = 0
+chc_boat_sink = 0
+chc_horse_fall = 0 
