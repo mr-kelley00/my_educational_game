7cb16bea5330e8727c4aafb3d79e3dd503ba3b87
Ryan Kelley <kelleyr@duvalschools.org>
2020-12-01 13:05:41 -0500

    [7B] v0.2
    Added inventory variables.

--- 7B/kelley_ryan_educational_game.py
+++ 7B/kelley_ryan_educational_game.py
@@ -1,4 +1,4 @@
-# <Educational Game>, <Ryan Kelley>, <12:52PM> <12/01/20>, <Version 0.1>
+# <Educational Game>, <Ryan Kelley>, <1:05PM> <12/01/20>, <Version 0.2>
 
 # Player Variables
 player_role = ""
@@ -10,3 +10,21 @@ party_member1 = ""
 party_member2 = ""
 party_member3 = ""
 party_member4 = ""
+
+
+# Inventory Variables
+amt_food = 0
+amt_water = 0
+amt_clothing = 0
+amt_wagons =
+amt_boats = 0
+amt_horses = 0
+num_guns = 0
+num_bullets = 0
+num_wagon_wheel = 0
+num_wagon_axle = 0
+num_boat_oar = 0
+num_pelts = 0
+
+
+
