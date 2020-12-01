cac0abc8e44d4ed4ef3c6045f618cb7c9b6c1171
Ryan Kelley <kelleyr@duvalschools.org>
2020-12-01 08:38:42 -0500

    [5B] v0.1
    Added variables related to the player.

--- 5B/kelley_ryan_educational_game.py
+++ 5B/kelley_ryan_educational_game.py
@@ -1,1 +1,11 @@
-# <The Lewis and Clark Expedition Simulator>, <Ryan Kelley>, <8:25AM> <12/01/20>, <Version 0.0>
+# <The Lewis and Clark Expedition Simulator>, <Ryan Kelley>, <8:38AM> <12/01/20>, <Version 0.1>
+
+# Player Variables
+starting_role = ""
+starting_money = 0
+score_bonus = 0
+player_name = ""
+party_member0 = ""
+party_member1 = ""
+party_member2 = ""
+party_member3 = ""
