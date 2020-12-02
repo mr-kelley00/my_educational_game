298fdd2cb1f580bf3891f2a22be859441295df0a
Ryan Kelley <kelleyr@duvalschools.org>
2020-12-02 12:19:35 -0500

    [3A] v0.2
    Added more inventory variables, organized variables into groups for Player, Inventory, Time/Date/Distance.

--- 3A/kelley_ryan_educational_game.py
+++ 3A/kelley_ryan_educational_game.py
@@ -1,6 +1,8 @@
-# <Lewis and Clark Expedition Extravaganza>, <Ryan Kelley>, <11/30/20> <1:17PM>, <Version 0.1>
+# <Lewis and Clark Expedition Extravaganza>, <Ryan Kelley>, <12/02/20> <12:17PM>, <Version 0.2>
 
 # Global Variables
+
+# Player Variables 
 player_score = 0
 player_name = ""
 party_member0 = ""
@@ -9,11 +11,23 @@ party_member2 = ""
 party_member3 = ""
 player_job = ""
 start_money = 0
+
+# Inventory Variables 
 num_boats = 0
 num_guns = 0
 num_bullets = 0
 amount_food = 0
 amount_water = 0
+num_wagons = 0
+num_horses = 0
+num_wagon_wheel = 0
+num_wagon_axle = 0
+num_clothing = 0
+num_pelts = 0
+num_traps = 0 
+
+
+# Time / Date / Distance Variables 
 game_time = 0
 game_date = 0
 landmark_distance = 0
