2c4ea235c0a5c3200d995b2b7fc7d82efb3b4154
Ryan Kelley <kelleyr@duvalschools.org>
2020-12-02 13:28:30 -0500

    [8B] v0.3
    Added main_menu() functions.

--- 8B/kelley_ryan_educational_game.py
+++ 8B/kelley_ryan_educational_game.py
@@ -1,4 +1,4 @@
-# <Educational Game>, <Ryan Kelley>, <2:12PM> <12/01/20>, <Version 0.2>
+# <Educational Game>, <Ryan Kelley>, <2:35PM> <12/01/20>, <Version 0.3>
 
 # Player Variables
 player_name = ""
@@ -23,3 +23,36 @@ num_pelts = 0
 num_beads = 0
 num_traps = 0
 money = 0 
+
+# Main Menu
+def main_menu():
+    print("+=====++=====++=====++=====++=====++=====++=====++=====++=====+")
+    print("+                          Ryan Kelley Games                  +")
+    print("+                                presents                     +")
+    print("+                           Lewis and Clark                   +")
+    print("+                                 in                          +")
+    print("+                        \"WHERE THE HECK ARE WE?\"             +")
+    print("+                                                             +")
+    print("+    Please choose an option from the menu.                   +")
+    print("+    1. Begin your journey.                                   +")
+    print("+    2. Learn more about the journey.                         +")
+    print("+    3. See the high scores.                                  +")
+    print("+    4. Enable / Disable Sound                                +")
+    print("+    5. Exit Game                                             +")
+    print("+                                                             +")
+    print("+=====++=====++=====++=====++=====++=====++=====++=====++=====+")
+    player_choice = int(input("Please type your choice and press enter.  \n"))
+    if player_choice == 1:
+        do_this
+    elif player_choice == 2:
+        do_that
+    elif player_choice == 3:
+        do_this_other_thing
+    elif player_choice == 4:
+        do_something_else
+    else:
+        exit() 
+
+main_menu()
+    
+    
