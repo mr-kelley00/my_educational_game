816362762152034dd5346e02052c8739101f0e71
Ryan Kelley <kelleyr@duvalschools.org>
2020-12-03 08:35:52 -0500

    [5B] v0.35
    Updated disaster variables to float values.
    
    Started main_menu() function.

--- 5B/kelley_ryan_educational_game.py
+++ 5B/kelley_ryan_educational_game.py
@@ -1,4 +1,4 @@
-# <The Lewis and Clark Expedition Simulator>, <Ryan Kelley>, <9:04AM> <12/01/20>, <Version 0.3>
+# <The Lewis and Clark Expedition Simulator>, <Ryan Kelley>, <8:33AM> <12/03/20>, <Version 0.35>
 
 # Player Variables
 starting_role = ""
@@ -24,10 +24,33 @@ num_wagon_wheel = 0
 num_wagon_axle = 0
 
 # Disaster Variables, % chance it occurs.  
-chc_sick = 0
-chc_break_wheel = 0
-chc_break_axle = 0
-chc_hostile_native = 0
-chc_bad_weather = 0
-chc_boat_sink = 0
-chc_horse_fall = 0 
+chc_sick = 0.
+chc_break_wheel = 0.0
+chc_break_axle = 0.0
+chc_hostile_native = 0.0
+chc_bad_weather = 0.0
+chc_boat_sink = 0.0
+chc_horse_fall = 0.0
+
+# Main Menu Function
+
+def main_menu():
+    print("""
+        +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+        *                        Ryan Kelley Game Studios           *
+        *                               presents                    *
+        *                           Lewis and Clark                 *
+        *                                 in                        *
+        *                           Are We There Yet?!              *
+        *                                                           *
+        *   1. Start your journey.                                  *
+        *   2. Learn more about the expedition.                     *
+        *   3. See the high scores.                                 *
+        *   4. Exit game.                                           *
+        *                                                           *
+        +=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+=+
+        """)
+
+
+
+
