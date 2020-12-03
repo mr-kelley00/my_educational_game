fb7fed9aa38e9b8c6935341bf297ff2b4f4701e3
Ryan Kelley <kelleyr@duvalschools.org>
2020-12-03 12:47:42 -0500

    [7B] v0.35
    Added main_menu() function.

--- 7B/kelley_ryan_educational_game.py
+++ 7B/kelley_ryan_educational_game.py
@@ -1,4 +1,4 @@
-# <Educational Game>, <Ryan Kelley>, <12:21PM> <12/03/20>, <Version 0.3>
+# <Educational Game>, <Ryan Kelley>, <12:47PM> <12/03/20>, <Version 0.35>
 
 # Player Variables
 player_role = ""
@@ -11,12 +11,11 @@ party_member2 = ""
 party_member3 = ""
 party_member4 = ""
 
-
 # Inventory Variables
 amt_food = 0
 amt_water = 0
 amt_clothing = 0
-amt_wagons =
+amt_wagons = 0 
 amt_boats = 0
 amt_horses = 0
 num_guns = 0
@@ -25,7 +24,6 @@ num_wagon_wheel = 0
 num_wagon_axle = 0
 num_boat_oar = 0
 num_pelts = 0
-
 
 # Disaster Variables, as a percentage chance of happening. 
 chnc_sick = 0.0
@@ -37,3 +35,26 @@ chnc_bear_attack = 0.0
 chnc_bandit_attack = 0.0
 chnc_mutiny = 0.0
 chnc_lost_path = 0.0 
+
+# Main Menu Function
+def main_menu():
+    print("""
+        [=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]
+        [                  Ryan Kelley Game Studios           ]
+        [                          presents                   ]
+        [                      Lewis and Clark                ]
+        [                             in                      ]
+        [                      ARE WE THERE YET?              ]
+        [                                                     ]
+        [    1.  Start the Journey                            ]
+        [    2.  Learn More About the Expedition              ]
+        [    3.  Check the High Scores                        ]
+        [    4.  Exit the Game                                ]
+        [                                                     ]
+        [                                                     ]
+        [=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=]
+        """)
+
+main_menu() # This is known as CALLING the function.     
+
+
