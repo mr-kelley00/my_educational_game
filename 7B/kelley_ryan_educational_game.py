66459e5e59c590853dfc308935b522363602e790
Ryan Kelley <kelleyr@duvalschools.org>
2020-12-07 12:46:43 -0500

    [7B] v0.47
    Added player_info() function to get player name and party member names.

--- 7B/kelley_ryan_educational_game.py
+++ 7B/kelley_ryan_educational_game.py
@@ -1,4 +1,4 @@
-# <Educational Game>, <Ryan Kelley>, <1:10PM> <12/03/20>, <Version 0.45>
+# <Educational Game>, <Ryan Kelley>, <12:44PM> <12/03/20>, <Version 0.47>
 
 # Player Variables
 player_role = ""
@@ -8,8 +8,6 @@ player_name = ""
 party_member0 = ""
 party_member1 = ""
 party_member2 = ""
-party_member3 = ""
-party_member4 = ""
 
 # Inventory Variables
 amt_food = 0
@@ -66,11 +64,24 @@ def main_menu():
         exit()
     return player_choice
 
-main_menu() # This is known as CALLING the function.     
+# main_menu() # This is known as CALLING the function.     
 
 # Display Info. Function
 def disp_info():
     print("""
         You need to write a PARAGRAPH in your OWN WORDS that provides information about your
         expedition.
-        """) 
+        """)
+
+# Player Info. Function
+def player_info():
+    player_name = input("Please give me your name brave adventurer.  What should I call you?\n")
+    print(f"Wow!  I have never met anyone named {player_name}.  It's great to meet you!\n")
+    # Next few lines of code will need to get the names of the other people in the party.
+    party_member0 = input("You have another joining you.  What is their name?\n")
+    party_member1 = input("I see you have a third member of the party.  What should I call them?\n")
+    party_member2 = input("Four people will be with you?  By what name are they known?\n")
+    print(f"{player_name} will joined by {party_member0}, {party_member1}, and {party_member2}.\n")
+    
+    
+player_info() 
