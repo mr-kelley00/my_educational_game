cd0865494b103ceaa372cad90368a8162f443c1d
Ryan Kelley <kelleyr@duvalschools.org>
2021-01-14 14:30:41 -0500

    [4A] v1.0.2
    Added location variables.
    
    Changed start_money to money.
    
    Used if/elif/else to check current_location and assign store_name and cost_multi.
    
    print() menu of items.

--- 4A/kelley_ryan_educational_game.py
+++ 4A/kelley_ryan_educational_game.py
@@ -1,8 +1,8 @@
-# <The Lewis and Clark Expedition>, <Ryan Kelley>, <2:45PM> <01/12/21>, <Version 1.0.1>
+# <The Lewis and Clark Expedition>, <Ryan Kelley>, <2:29PM> <01/14/21>, <Version 1.0.2>
 
 # Player Variables
 occupation = "" 
-start_money = 0
+money = 0
 score_bonus = 0
 player_name = ""
 party_member0 = ""
@@ -16,8 +16,6 @@ amount_water = 0
 num_boats = 0
 num_guns = 0
 num_bullets = 0
-num_swords = 0
-num_axes = 0
 num_horses = 0
 num_wagons = 0
 num_clothing = 0
@@ -31,8 +29,6 @@ cost_water = 0
 cost_boats = 0
 cost_guns = 0
 cost_bullets = 0
-cost_swords = 0
-cost_axes = 0
 cost_horses = 0
 cost_wagons = 0
 cost_clothing = 0
@@ -40,6 +36,13 @@ cost_cows = 0
 cost_chickens = 0
 cost_pigs = 0
 
+# Location Variables
+starting_point = "St. Louis, Missouri"
+ending_point = "Fort Calstop"
+location0 = "Test 1"
+location1 = "Test 2"
+location2 = "Test 3"
+current_location = starting_point 
 
 
 # Disaster Variables
@@ -90,7 +93,7 @@ def disp_info():
 # Player Info Function
 def player_info():
     global player_name, party_member0, party_member1, party_member2, party_member3
-    global score_bonus, start_money, occupation
+    global score_bonus, money, occupation
     
     player_name = input("What should I call you?\n")
     print(f"{player_name} is a mighty fine name.  What about the other people in your group?\n")
@@ -110,20 +113,20 @@ def player_info():
     if occupation == "Scout" or occupation == "scout" or occupation == "SCOUT":
         print("You have chosen the scout role.  You will get the most amount of money and highest score bonus.\n") 
         score_bonus = 3
-        start_money = 1000
+        money = 1000
     elif occupation == "Cook" or occupation == "cook" or occupation == "COOK":
         print("You have chosen the cook role.  You will get a higher amount of money and score bonus.\n")
         score_bonus = 2
-        start_money = 750
+        money = 750
     elif occupation == "Trapper" or occupation == "trapper" or occupation == "TRAPPER":
         print("You have chosen the trapper role.  You will get more money and small score bonus.\n")
         score_bonus = 1
-        start_money = 500
+        money = 500
     else:
         print("You did not choose a job from the list so you will be a dishwasher.  You get nothing.\n")
         score_bonus = 0
-        start_money = 250
-    print(f"{player_name}, you will have {start_money} DOLLARS / YEN / GOLD.\n")
+        money = 250
+    print(f"{player_name}, you will have {money} DOLLARS / YEN / GOLD.\n")
     print(f"You will also receive a score multiplier of {score_bonus} when you finish.\n")   
     
 # player_info()
@@ -147,28 +150,58 @@ show_inventory()
 # Trading Transactions
 
 def buy_item():
-    global num_bullets
-    global num_wagons
+        
     global amount_food
     global amount_water 
-    global num_boats 
     global num_guns 
     global num_bullets 
     global num_horses 
     global num_wagons 
+    global num_boats 
     global num_clothing 
     global num_cows 
     global num_chickens 
-    global num_pigs 
+    global num_pigs
+    global money 
 
     if current_location == "St. Louis, Missouri":
-        store_name = "Sally's St. Louis Super Mart!\n")
+        store_name = "Sally's St. Louis Super Mart!\n"
         cost_multi = 0.75
-    elif ?:
-        store_name =
-        cost_multi =
-    else:  
+    elif current_location == "Test 0":
+        store_name = "Test 0 Store Name\n"
+        cost_multi = 1.0
+    elif current_location == "Test 1":
+        store_name = "Test 1 Store Name\n"
+        cost_multi = 1.5
+    else:
+        store_name = "Test 2 Store Name\n"
+        cost_multi = 2.0
 
+    while True:
+        print(f"""
+                                {store_name}
+        Current Balance: ${money}
+        \n
+        +=============================================================+
+        |               Available Inventory for Sale                  |
+        |                                                             |
+        | 0) Food    1) Water    2) Guns  3) Bullets                  |
+        | 4) Horses  5) Wagons   6) Boat  7) Clothing                 |
+        | 8) Cows    7) Chickens 8) Pigs                              |
+        +=============================================================+
+        \n
+        """)
+
+        break
+
+buy_item() 
+
+
+
+        
+        
+        
+        
 
 
 
