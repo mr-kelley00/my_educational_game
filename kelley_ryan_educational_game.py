5cc2c04e29719fde95ce5fd1085162bc85e22223
Ryan Kelley <kelleyr@duvalschools.org>
2021-01-27 22:25:03 -0500

    [9A] v0.1.0
    Declared and initialzed player variables.

--- kelley_ryan_educational_game.py
+++ kelley_ryan_educational_game.py
@@ -1,1 +1,11 @@
-# <The Lewis and Clark Expedition>, <Ryan Kelley>, <10:16PM> <01/17/2021>, <Version 0.0.0>
+# <The Lewis and Clark Expedition>, <Ryan Kelley>, <10:16PM> <01/17/2021>, <Version 0.1.0>
+
+# Player Variables 
+starting_role = " "
+money = 0 
+score_bonus = 0 
+player_name = " "
+party_member0 = " "
+party_member1 = " "
+party_member2 = " "
+party_member3 = " "
