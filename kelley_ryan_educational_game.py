833121c365cd579005d765cbcd974973b000c625
Ryan Kelley <kelleyr@duvalschools.org>
2021-01-27 22:31:55 -0500

    [9A] v0.2.0
    Added inventory variables, assigned value of 0.

--- kelley_ryan_educational_game.py
+++ kelley_ryan_educational_game.py
@@ -1,4 +1,4 @@
-# <The Lewis and Clark Expedition>, <Ryan Kelley>, <10:16PM> <01/17/2021>, <Version 0.1.0>
+# <The Lewis and Clark Expedition>, <Ryan Kelley>, <10:31PM> <01/27/2021>, <Version 0.2.0>
 
 # Player Variables 
 starting_role = " "
@@ -9,3 +9,16 @@ party_member0 = " "
 party_member1 = " "
 party_member2 = " "
 party_member3 = " "
+
+# Inventory Variables 
+amt_water = 0 
+amt_food = 0
+num_clothing = 0
+num_guns = 0
+num_bullets = 0
+amt_rope = 0
+num_horses = 0
+num_boats = 0
+num_wagons = 0
+num_wagon_wheel = 0
+num_wagon_axle = 0
